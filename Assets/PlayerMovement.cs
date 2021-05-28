70f25d4e4fbac37bd2fe0b7db7196ad933c65fd5
WildernessBear <littlefoot93@gmail.com>
2021-05-28 00:32:49 -0400

    White plane, pink square and light pink sky

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, 200);
+    }
+}
