ab2895307084de9aed726c8526efd8547fa5c268
WildernessBear <55259033+WildernessBear@users.noreply.github.com>
2021-05-31 22:48:13 -0400

    updating to move files to gitignore

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // reference to the RigidBody component called "rb"
     public Rigidbody rb;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb.AddForce(0, 200, 500);
-    }
+    public float forwardForce = 20000f;
+    public float sidewaysForce = 500f;
 
-    // Update is called once per frame
-    void Update()
+    // use FixedUpdate when calculating physics
+    void FixedUpdate()
     {
-        rb.AddForce(0, 0, 200);
+        // add a forward force
+        // deltaTime is used to keep speed 
+        // constant no mater the frame rate
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
