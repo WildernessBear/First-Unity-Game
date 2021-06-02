92ddda0248436a0fae65acda5355a07b2742f7f6
WildernessBear <55259033+WildernessBear@users.noreply.github.com>
2021-06-02 00:00:17 -0400

    E06 - Gameplay
    Need to go back, prefabs weren't created correctly

---
+++ Assets/Materials/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // reference to the RigidBody component called "rb"
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // use FixedUpdate when calculating physics
+    void FixedUpdate()
+    {
+        // add a forward force
+        // deltaTime is used to keep speed 
+        // constant no mater the frame rate
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
